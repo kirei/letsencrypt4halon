939f890fd3e25b96852492ce13bfb041cc9e2e10
Jakob Schlyter <jakob@kirei.se>
2019-02-21 12:22:38 +0100

    fix payload bug, remove archive

--- halon-deploy-cert.py
+++ halon-deploy-cert.py
@@ -6,25 +6,26 @@ import base64
 from datetime import datetime, timezone
 
 
+
 # read configuration
 with open('halon-deploy-cert.json') as input_file:
     config = json.load(input_file)
 
-timestamp = datetime.now(tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S %z")
+timestamp = datetime.now(tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S%z")
 halon_url = f"https://{config['hostname']}/api/1.0.0"
 
 # read cert & key
 with open(config['certfile']) as cert_file:
-    pki_cert = cert_file.read()
+    pki_certdata = cert_file.read()
 with open(config['keyfile']) as key_file:
-    pki_key = key_file.read()
+    pki_keydata = key_file.read()
 
 # construct new PKI parameters
 pki_key = f"pki__{config['pki_key']}"
 pki_params = [
     f"{config['certname']} (certbot {timestamp})",
     "x509+privatekey",
-    base64.b64encode((pki_cert + pki_key).encode()).decode()
+    base64.b64encode((pki_certdata + pki_keydata).encode()).decode()
 ]
 
 # create session for Halon
@@ -58,14 +59,6 @@ for c in last_halon_config['config']:
 if config.get('deploy', False):
     response = session.post(f"{halon_url}/config/revisions/{next_halon_revision}",
                             json=next_halon_config)
-    print(response.status_code)
-    print(response.text)
     response.raise_for_status()
-
-# archive configuration
-archive_path = config.get('archive_path')
-if archive_path is not None:
-    with open(f"{archive_path}/halon-config-{last_halon_revision}.json", "wt") as f:
-        f.write(json.dumps(last_halon_config, indent=4))
-    with open(f"{archive_path}/halon-config-{next_halon_revision}.json", "wt") as f:
-        f.write(json.dumps(next_halon_config, indent=4))
+else:
+    print(json.dumps(next_halon_config, indent=4))
