8d8c31c507979465734f086fb75ef6213f3d7a93
Jakob Schlyter <jakob@kirei.se>
2020-04-15 08:03:07 +0200

    configurable output

--- halon-save-config.py
+++ halon-save-config.py
@@ -2,33 +2,58 @@
 
 """Fetch configuration from HALON and save as JSON"""
 
+import argparse
 import base64
 import json
 from datetime import datetime, timezone
 
 import requests
 
-# read configuration
-with open('halon-save-config.json') as input_file:
-    config = json.load(input_file)
 
-halon_url = f"https://{config['hostname']}/api/1.0.0"
+def main() -> None:
+    """Main functions"""
 
-# create session for Halon
-session = requests.Session()
-session.auth = (config['username'], config['password'])
-if 'cacert' in config:
-    session.verify = config['cacert']
+    parser = argparse.ArgumentParser(description="Save HALON config")
+    parser.add_argument(
+        "--config",
+        metavar="filename",
+        required=False,
+        help="Config file",
+        default="halon-save-config.json",
+    )
+    parser.add_argument(
+        "--output", metavar="filename", required=False, help="Output file"
+    )
+    args = parser.parse_args()
 
-# fetch configuration from Halon
-response = session.get(f"{halon_url}/config/revisions/HEAD")
-response.raise_for_status()
+    # read configuration
+    with open(args.config) as input_file:
+        config = json.load(input_file)
 
-# digest current configuration
-last_halon_config = response.json()
-last_halon_revision = last_halon_config['id']
+    halon_url = f"https://{config['hostname']}/api/1.0.0"
 
-# archive configurations
-archive_path = config.get('archive_path', '.')
-with open(f"{archive_path}/halon-config-{last_halon_revision}.json", "wt") as f:
-    f.write(json.dumps(last_halon_config, indent=4))
+    # create session for Halon
+    session = requests.Session()
+    session.auth = (config["username"], config["password"])
+    if "cacert" in config:
+        session.verify = config["cacert"]
+
+    # fetch configuration from Halon
+    response = session.get(f"{halon_url}/config/revisions/HEAD")
+    response.raise_for_status()
+
+    # digest current configuration
+    last_halon_config = response.json()
+    last_halon_revision = last_halon_config["id"]
+
+    # archive configurations
+    archive_path = config.get("archive_path", ".")
+    output_filename = (
+        args.output or f"{archive_path}/halon-config-{last_halon_revision}.json"
+    )
+    with open(output_filename, "wt") as f:
+        f.write(json.dumps(last_halon_config, indent=4))
+
+
+if __name__ == "__main__":
+    main()
